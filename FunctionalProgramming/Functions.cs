d0de9a9871300e7e95223e75cc0ecc25b6bc0b09
kchipp <khipp2@gmail.com>
2016-09-12 10:14:18 -0500

    functions added

---
+++ FunctionalProgramming/Functions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    class Functions
+    {
+        public Functions()//constructor
+        {
+
+        }
+
+        public void AverageGrades()
+        {
+            string scores = "90, 65, 100, 82, 89, 55, 69";
+
+            List<double> list = new List<double>();
+
+            double score;
+
+            var scoreNumbers = scores.Split(new[] { ',' })
+                .Select(scoreToString => double.TryParse(scoreToString, out score) ? score : 0)
+                .ToList()
+                .OrderBy(scoresValue => scoresValue).Skip(3);
+
+            double averageGrade = scoreNumbers.Average();
+
+            Console.WriteLine($"Average {averageGrade}");
+
+
+        }
+
+        public void CountLetters()
+        {
+            string word = "GoPackGo";
+            word = string.Concat(word.ToUpper().OrderBy(a => a));
+            var sortString = word.GroupBy(b => b)
+                .Select(collect => new { Char = collect.Key, Count = collect.Count() });
+
+            foreach (var letter in sortString)
+            {
+                Console.Write($"{letter.Char}{letter.Count}");
+            };
+
+        }
+
+
+    }//class
+}//namespace
---
+++ FunctionalProgramming/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Functions functions = new Functions();
+            Console.WriteLine("Grades to Average(minus lowest 3): 90, 65, 100, 82, 89, 55, 69");
+            functions.AverageGrades();
+            Console.WriteLine("To be counted: Go Pack Go");
+            functions.CountLetters();
+            Console.ReadLine();
+        }
+    }
+}
